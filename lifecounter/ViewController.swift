9ac1ff8c32185ab2a4b3d8dd04dfddb810bf03f0
Keegan Hanes <keeganhanes4@gmail.com>
2019-01-28 10:41:40 -0800

    Add loser message

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -11,13 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var p1Life: UILabel!
-    var p1LifeValue = 0
+    var p1LifeValue : Int = 0
     @IBOutlet weak var p2Life: UILabel!
-    var p2LifeValue = 0
+    var p2LifeValue : Int = 0
     @IBOutlet weak var p3Life: UILabel!
-    var p3LifeValue = 0
+    var p3LifeValue : Int = 0
     @IBOutlet weak var p4Life: UILabel!
-    var p4LifeValue = 0
+    var p4LifeValue : Int = 0
+    @IBOutlet weak var playerLostLabel: UILabel!
+    
+    public func didPlayerLose(_ life: Int, playerNum: String) {
+        if life <= 0 {
+            playerLostLabel.text = "Player \(playerNum) LOSES!"
+        }
+    }
     
     @IBAction func p1Add(_ sender: Any) {
         p1LifeValue = p1LifeValue + 1
@@ -32,11 +39,13 @@ class ViewController: UIViewController {
     @IBAction func p1Subtract(_ sender: Any) {
         p1LifeValue = p1LifeValue - 1
         p1Life.text = String(p1LifeValue)
+        didPlayerLose(p1LifeValue, playerNum: "one")
     }
     
     @IBAction func p1Subtract5(_ sender: Any) {
         p1LifeValue = p1LifeValue - 5
         p1Life.text = String(p1LifeValue)
+        didPlayerLose(p1LifeValue, playerNum: "one")
     }
     
     @IBAction func p2Add(_ sender: Any) {
@@ -47,11 +56,13 @@ class ViewController: UIViewController {
     @IBAction func p2Add5(_ sender: Any) {
         p2LifeValue = p2LifeValue - 5
         p2Life.text = String(p2LifeValue)
+        didPlayerLose(p2LifeValue, playerNum: "two")
     }
     
     @IBAction func p2Subtract(_ sender: Any) {
         p2LifeValue = p2LifeValue - 1
         p2Life.text = String(p2LifeValue)
+        didPlayerLose(p2LifeValue, playerNum: "two")
     }
     
     @IBAction func p2Subtract5(_ sender: Any) {
@@ -62,6 +73,7 @@ class ViewController: UIViewController {
     @IBAction func p3Subtract(_ sender: Any) {
         p3LifeValue = p3LifeValue - 1
         p3Life.text = String(p3LifeValue)
+        didPlayerLose(p3LifeValue, playerNum: "three")
     }
     
     @IBAction func p3Add(_ sender: Any) {
@@ -72,6 +84,7 @@ class ViewController: UIViewController {
     @IBAction func p3Subtract5(_ sender: Any) {
         p3LifeValue = p3LifeValue - 5
         p3Life.text = String(p3LifeValue)
+        didPlayerLose(p3LifeValue, playerNum: "three")
     }
     
     @IBAction func p3Add5(_ sender: Any) {
@@ -82,6 +95,7 @@ class ViewController: UIViewController {
     @IBAction func p4Subtract(_ sender: Any) {
         p4LifeValue = p4LifeValue - 1
         p4Life.text = String(p4LifeValue)
+        didPlayerLose(p4LifeValue, playerNum: "four")
     }
     
     @IBAction func p4Add(_ sender: Any) {
@@ -92,6 +106,7 @@ class ViewController: UIViewController {
     @IBAction func p4Subtract5(_ sender: Any) {
         p4LifeValue = p4LifeValue - 5
         p4Life.text = String(p4LifeValue)
+        didPlayerLose(p4LifeValue, playerNum: "four")
     }
     
     @IBAction func p4Add5(_ sender: Any) {
